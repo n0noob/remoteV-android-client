beb650c8ea3ce86d8695d100c1ca737c54017614
n0noob <neemanoop@gmail.com>
2016-04-12 00:42:09 +0530

    added mediafile

---
+++ MediaFile.java
@@ -0,0 +1,24 @@
+package com.example.anoop.tcp_client;
+
+/**
+ * Created by Anoop on 4/9/2016.
+ */
+
+public class MediaFile {
+    String file;
+
+    public MediaFile(){
+    }
+
+    public MediaFile(String f){
+        file = new String(f);
+    }
+
+    public String get_file(){
+        return file;
+    }
+
+    public void extract_and_set(String raw){
+        //
+    }
+}

1e6a1e8267c354099c7942923de0f9a51c81dcba
n0noob <neemanoop@gmail.com>
2016-04-14 00:13:32 +0530

    added screencontrols

---
+++ ScreenControls.java
@@ -0,0 +1,13 @@
+package com.example.anoop.tcp_client;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ScreenControls extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_screen_controls);
+    }
+}
